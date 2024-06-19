bd3b837ab446a5c1c3a9a5c806a1e00899b20d1c
chand kaurati <132880291+chandkaurati@users.noreply.github.com>
2024-06-19 16:29:40 +0000

    create user api

--- RestApi/src/index.js
+++ RestApi/src/index.js
@@ -1,12 +1,22 @@
-import express from "express"
+const express = require("express");
+const data = require("./Data/MOCK_DATA.json");
+// import usersData from "./Data/MOCK_DATA.json";
+const app = express();
+const port = 8000;
 
-const app = express()
-const port = 8000
+app.get("/", (req, res) => {
+  res.send("welcome rest api ");
+});
+app.get("/api/users", (req, res) => {
+  res.json(data);
+});
+app.get("/api/users/:id?", (req, res) => {
+  const user = data.find((el) => el.id == req.params.id);
+  if (!user) res.send("user Not found");
 
-app.get("/",(req,res)=>{
-    res.send("welcome rest api ")
-})
-app.listen(port,()=>{
-    "server started"
-})
+  res.json(user);
+});
 
+app.listen(port, () => {
+  console.log("server started");
+});

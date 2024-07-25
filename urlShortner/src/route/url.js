f95fc5e1179647b3152bf57864a4ebe95f369d44
chandkaurati <chandkaurati@outlook.com>
2024-07-25 16:23:34 +0530

    removed Project

--- urlShortner/src/route/url.js
+++
@@ -1,5 +0,0 @@
-const express = require('express')
-const {handleGenrateNewShortUrl} = require("../controller/url_controller")
-const router = express.Router()
-
-router.post('/', handleGenrateNewShortUrl)
